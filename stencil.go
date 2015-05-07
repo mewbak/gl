5d55a54a535fae7623af1886c603df6b0973184d
Olivier Gagnon <oli.gagnon4418@gmail.com>
2015-05-07 12:40:28 -0400

    added stencil funcs

---
+++ stencil.go
@@ -0,0 +1,64 @@
+package gl
+
+import (
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+type StencilObj struct{}
+
+var Stencil = StencilObj{}
+
+//Enable is an alias to gl.Enable(gl.STENCIL_TEST)
+func (StencilObj) Enable() {
+	gl.Enable(gl.STENCIL_TEST)
+}
+
+//Disable is an alias to gl.Disable(gl.STENCIL_TEST)
+func (StencilObj) Disable() {
+	gl.Disable(gl.STENCIL_TEST)
+}
+
+//Func is an alias to gl.StencilFunc(f, ref, mask)
+func (StencilObj) Func(f StencilFunc, ref int32, mask uint32) {
+	gl.StencilFunc(uint32(f), ref, mask)
+}
+
+//Op is an alias to gl.StencilOp(sfail, zfail, zpass)
+func (StencilObj) Op(sfail, zfail, zpass StencilOp) {
+	gl.StencilOp(uint32(sfail), uint32(zfail), uint32(zpass))
+}
+
+//Mask is an alias to gl.StencilMask(mask)
+func (StencilObj) Mask(mask uint32) {
+	gl.StencilMask(mask)
+}
+
+//Enum to represent all possible stencil func, prevents bad arguments
+type StencilFunc uint32
+
+//Stencil func possible values
+const (
+	Never    StencilFunc = gl.NEVER
+	Less                 = gl.LESS
+	Lequal               = gl.LEQUAL
+	Greater              = gl.GREATER
+	Gequal               = gl.GEQUAL
+	Equal                = gl.EQUAL
+	NotEqual             = gl.NOTEQUAL
+	Always               = gl.ALWAYS
+)
+
+//Enum to represent all possible stencil op, prevent bad arguments
+type StencilOp uint32
+
+//Stencil op possible values
+const (
+	Keep     StencilOp = gl.KEEP
+	Zero               = gl.ZERO
+	Replace            = gl.REPLACE
+	Incr               = gl.INCR
+	IncrWrap           = gl.INCR_WRAP
+	Decr               = gl.DECR
+	DecrWrap           = gl.DECR_WRAP
+	Invert             = gl.INVERT
+)
--- uniformlocation.go
+++ uniformlocation.go
@@ -4,170 +4,172 @@ import (
 	"github.com/go-gl/gl/v3.3-core/gl"
 )
 
-//UniformLocation is the high-level representation of openGL shader uniform location, generated via the program object.
+//UniformLocation is the high-level representation of openGL shader uniform location, generated via the program object. The goal is to specify exactly which type this variable is so that you can restrict the Go compiler on what function you can use on this variable.
+//    mvp := program.GetUniformLocation("MVP").ToMatrix4()
+//    //you can only call UniformMatrix4fv on mvp
 type UniformLocation int32
 
-//Uniform1f is an alias for the Opengl function of the same name.
+//Uniform1f is an alias to glUniform1f.
 func (ul UniformLocation) Uniform1f(v0 float32) {
 	gl.Uniform1f(int32(ul), v0)
 }
 
-//Uniform2f is an alias for the Opengl function of the same name.
+//Uniform2f is an alias to glUniform2f.
 func (ul UniformLocation) Uniform2f(v0, v1 float32) {
 	gl.Uniform2f(int32(ul), v0, v1)
 }
 
-//Uniform3f is an alias for the Opengl function of the same name.
+//Uniform3f is an alias to glUniform3f.
 func (ul UniformLocation) Uniform3f(v0, v1, v2 float32) {
 	gl.Uniform3f(int32(ul), v0, v1, v2)
 }
 
-//Uniform4f is an alias for the Opengl function of the same name.
+//Uniform4f is an alias to glUniform4f.
 func (ul UniformLocation) Uniform4f(v0, v1, v2, v3 float32) {
 	gl.Uniform4f(int32(ul), v0, v1, v2, v3)
 }
 
-//Uniform1i is an alias for the Opengl function of the same name.
+//Uniform1i is an alias to glUniform1i.
 func (ul UniformLocation) Uniform1i(v0 int32) {
 	gl.Uniform1i(int32(ul), v0)
 }
 
-//Uniform2i is an alias for the Opengl function of the same name.
+//Uniform2i is an alias to glUniform2i.
 func (ul UniformLocation) Uniform2i(v0, v1 int32) {
 	gl.Uniform2i(int32(ul), v0, v1)
 }
 
-//Uniform3i is an alias for the Opengl function of the same name.
+//Uniform3i is an alias to glUniform3i.
 func (ul UniformLocation) Uniform3i(v0, v1, v2 int32) {
 	gl.Uniform3i(int32(ul), v0, v1, v2)
 }
 
-//Uniform4i is an alias for the Opengl function of the same name.
+//Uniform4i is an alias to glUniform4i.
 func (ul UniformLocation) Uniform4i(v0, v1, v2, v3 int32) {
 	gl.Uniform4i(int32(ul), v0, v1, v2, v3)
 }
 
-//Uniform1ui is an alias for the Opengl function of the same name.
+//Uniform1ui is an alias to glUniform1ui.
 func (ul UniformLocation) Uniform1ui(v0 uint32) {
 	gl.Uniform1ui(int32(ul), v0)
 }
 
-//Uniform2ui is an alias for the Opengl function of the same name.
+//Uniform2ui is an alias to glUniform2ui.
 func (ul UniformLocation) Uniform2ui(v0, v1 uint32) {
 	gl.Uniform2ui(int32(ul), v0, v1)
 }
 
-//Uniform3ui is an alias for the Opengl function of the same name.
+//Uniform3ui is an alias to glUniform3ui.
 func (ul UniformLocation) Uniform3ui(v0, v1, v2 uint32) {
 	gl.Uniform3ui(int32(ul), v0, v1, v2)
 }
 
-//Uniform4ui is an alias for the Opengl function of the same name.
+//Uniform4ui is an alias to glUniform4ui.
 func (ul UniformLocation) Uniform4ui(v0, v1, v2, v3 uint32) {
 	gl.Uniform4ui(int32(ul), v0, v1, v2, v3)
 }
 
-//Uniform1fv is an alias for the Opengl function of the same name.
+//Uniform1fv is an alias to glUniform1fv.
 func (ul UniformLocation) Uniform1fv(count int32, value *float32) {
 	gl.Uniform1fv(int32(ul), count, value)
 }
 
-//Uniform2fv is an alias for the Opengl function of the same name.
+//Uniform2fv is an alias to glUniform2fv.
 func (ul UniformLocation) Uniform2fv(count int32, value *float32) {
 	gl.Uniform2fv(int32(ul), count, value)
 }
 
-//Uniform3fv is an alias for the Opengl function of the same name.
+//Uniform3fv is an alias to glUniform3fv.
 func (ul UniformLocation) Uniform3fv(count int32, value *float32) {
 	gl.Uniform3fv(int32(ul), count, value)
 }
 
-//Uniform4fv is an alias for the Opengl function of the same name.
+//Uniform4fv is an alias to glUniform4fv.
 func (ul UniformLocation) Uniform4fv(count int32, value *float32) {
 	gl.Uniform4fv(int32(ul), count, value)
 }
 
-//Uniform1iv is an alias for the Opengl function of the same name.
+//Uniform1iv is an alias to glUniform1iv.
 func (ul UniformLocation) Uniform1iv(count int32, value *int32) {
 	gl.Uniform1iv(int32(ul), count, value)
 }
 
-//Uniform2iv is an alias for the Opengl function of the same name.
+//Uniform2iv is an alias to glUniform2iv.
 func (ul UniformLocation) Uniform2iv(count int32, value *int32) {
 	gl.Uniform2iv(int32(ul), count, value)
 }
 
-//Uniform3iv is an alias for the Opengl function of the same name.
+//Uniform3iv is an alias to glUniform3iv.
 func (ul UniformLocation) Uniform3iv(count int32, value *int32) {
 	gl.Uniform3iv(int32(ul), count, value)
 }
 
-//Uniform4iv is an alias for the Opengl function of the same name.
+//Uniform4iv is an alias to glUniform4iv.
 func (ul UniformLocation) Uniform4iv(count int32, value *int32) {
 	gl.Uniform4iv(int32(ul), count, value)
 }
 
-//Uniform1uiv is an alias for the Opengl function of the same name.
+//Uniform1uiv is an alias to glUniform1uiv.
 func (ul UniformLocation) Uniform1uiv(count int32, value *uint32) {
 	gl.Uniform1uiv(int32(ul), count, value)
 }
 
-//Uniform2uiv is an alias for the Opengl function of the same name.
+//Uniform2uiv is an alias to glUniform2uiv.
 func (ul UniformLocation) Uniform2uiv(count int32, value *uint32) {
 	gl.Uniform2uiv(int32(ul), count, value)
 }
 
-//Uniform3uiv is an alias for the Opengl function of the same name.
+//Uniform3uiv is an alias to glUniform3uiv.
 func (ul UniformLocation) Uniform3uiv(count int32, value *uint32) {
 	gl.Uniform3uiv(int32(ul), count, value)
 }
 
-//Uniform4uiv is an alias for the Opengl function of the same name.
+//Uniform4uiv is an alias to glUniform4uiv.
 func (ul UniformLocation) Uniform4uiv(count int32, value *uint32) {
 	gl.Uniform4uiv(int32(ul), count, value)
 }
 
-//UniformMatrix2fv is an alias for the Opengl function of the same name.
+//UniformMatrix2fv is an alias to glUniformMatrix2fv.
 func (ul UniformLocation) UniformMatrix2fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix2fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix3fv is an alias for the Opengl function of the same name.
+//UniformMatrix3fv is an alias to glUniformMatrix3fv.
 func (ul UniformLocation) UniformMatrix3fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix3fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix4fv is an alias for the Opengl function of the same name.
+//UniformMatrix4fv is an alias to glUniformMatrix4fv.
 func (ul UniformLocation) UniformMatrix4fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix4fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix2x3fv is an alias for the Opengl function of the same name.
+//UniformMatrix2x3fv is an alias to glUniformMatrix2x3fv.
 func (ul UniformLocation) UniformMatrix2x3fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix2x3fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix3x2fv is an alias for the Opengl function of the same name.
+//UniformMatrix3x2fv is an alias to glUniformMatrix3x2fv.
 func (ul UniformLocation) UniformMatrix3x2fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix3x2fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix2x4fv is an alias for the Opengl function of the same name.
+//UniformMatrix2x4fv is an alias to glUniformMatrix2x4fv.
 func (ul UniformLocation) UniformMatrix2x4fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix2x4fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix4x2fv is an alias for the Opengl function of the same name.
+//UniformMatrix4x2fv is an alias to glUniformMatrix4x2fv.
 func (ul UniformLocation) UniformMatrix4x2fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix4x2fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix3x4fv is an alias for the Opengl function of the same name.
+//UniformMatrix3x4fv is an alias to glUniformMatrix3x4fv.
 func (ul UniformLocation) UniformMatrix3x4fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix3x4fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix4x3fv is an alias for the Opengl function of the same name.
+//UniformMatrix4x3fv is an alias to glUniformMatrix4x3fv.
 func (ul UniformLocation) UniformMatrix4x3fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix4x3fv(int32(ul), count, transpose, value)
 }
@@ -344,167 +346,167 @@ func (ul UniformLocation) ToMatrix4x3() UniformLocationMatrix4x3 {
 	return UniformLocationMatrix4x3(ul)
 }
 
-//Uniform1f is an alias for the Opengl function of the same name.
+//Uniform1f is an alias to glUniform1f.
 func (ul UniformLocation1f) Uniform1f(v0 float32) {
 	gl.Uniform1f(int32(ul), v0)
 }
 
-//Uniform2f is an alias for the Opengl function of the same name.
+//Uniform2f is an alias to glUniform2f.
 func (ul UniformLocation2f) Uniform2f(v0, v1 float32) {
 	gl.Uniform2f(int32(ul), v0, v1)
 }
 
-//Uniform3f is an alias for the Opengl function of the same name.
+//Uniform3f is an alias to glUniform3f.
 func (ul UniformLocation3f) Uniform3f(v0, v1, v2 float32) {
 	gl.Uniform3f(int32(ul), v0, v1, v2)
 }
 
-//Uniform4f is an alias for the Opengl function of the same name.
+//Uniform4f is an alias to glUniform4f.
 func (ul UniformLocation4f) Uniform4f(v0, v1, v2, v3 float32) {
 	gl.Uniform4f(int32(ul), v0, v1, v2, v3)
 }
 
-//Uniform1i is an alias for the Opengl function of the same name.
+//Uniform1i is an alias to glUniform1i.
 func (ul UniformLocation1i) Uniform1i(v0 int32) {
 	gl.Uniform1i(int32(ul), v0)
 }
 
-//Uniform2i is an alias for the Opengl function of the same name.
+//Uniform2i is an alias to glUniform2i.
 func (ul UniformLocation2i) Uniform2i(v0, v1 int32) {
 	gl.Uniform2i(int32(ul), v0, v1)
 }
 
-//Uniform3i is an alias for the Opengl function of the same name.
+//Uniform3i is an alias to glUniform3i.
 func (ul UniformLocation3i) Uniform3i(v0, v1, v2 int32) {
 	gl.Uniform3i(int32(ul), v0, v1, v2)
 }
 
-//Uniform4i is an alias for the Opengl function of the same name.
+//Uniform4i is an alias to glUniform4i.
 func (ul UniformLocation4i) Uniform4i(v0, v1, v2, v3 int32) {
 	gl.Uniform4i(int32(ul), v0, v1, v2, v3)
 }
 
-//Uniform1ui is an alias for the Opengl function of the same name.
+//Uniform1ui is an alias to glUniform1ui.
 func (ul UniformLocation1ui) Uniform1ui(v0 uint32) {
 	gl.Uniform1ui(int32(ul), v0)
 }
 
-//Uniform2ui is an alias for the Opengl function of the same name.
+//Uniform2ui is an alias to glUniform2ui.
 func (ul UniformLocation2ui) Uniform2ui(v0, v1 uint32) {
 	gl.Uniform2ui(int32(ul), v0, v1)
 }
 
-//Uniform3ui is an alias for the Opengl function of the same name.
+//Uniform3ui is an alias to glUniform3ui.
 func (ul UniformLocation3ui) Uniform3ui(v0, v1, v2 uint32) {
 	gl.Uniform3ui(int32(ul), v0, v1, v2)
 }
 
-//Uniform4ui is an alias for the Opengl function of the same name.
+//Uniform4ui is an alias to glUniform4ui.
 func (ul UniformLocation4ui) Uniform4ui(v0, v1, v2, v3 uint32) {
 	gl.Uniform4ui(int32(ul), v0, v1, v2, v3)
 }
 
-//Uniform1fv is an alias for the Opengl function of the same name.
+//Uniform1fv is an alias to glUniform1fv.
 func (ul UniformLocation1f) Uniform1fv(count int32, value *float32) {
 	gl.Uniform1fv(int32(ul), count, value)
 }
 
-//Uniform2fv is an alias for the Opengl function of the same name.
+//Uniform2fv is an alias to glUniform2fv.
 func (ul UniformLocation2f) Uniform2fv(count int32, value *float32) {
 	gl.Uniform2fv(int32(ul), count, value)
 }
 
-//Uniform3fv is an alias for the Opengl function of the same name.
+//Uniform3fv is an alias to glUniform3fv.
 func (ul UniformLocation3f) Uniform3fv(count int32, value *float32) {
 	gl.Uniform3fv(int32(ul), count, value)
 }
 
-//Uniform4fv is an alias for the Opengl function of the same name.
+//Uniform4fv is an alias to glUniform4fv.
 func (ul UniformLocation4f) Uniform4fv(count int32, value *float32) {
 	gl.Uniform4fv(int32(ul), count, value)
 }
 
-//Uniform1iv is an alias for the Opengl function of the same name.
+//Uniform1iv is an alias to glUniform1iv.
 func (ul UniformLocation1i) Uniform1iv(count int32, value *int32) {
 	gl.Uniform1iv(int32(ul), count, value)
 }
 
-//Uniform2iv is an alias for the Opengl function of the same name.
+//Uniform2iv is an alias to glUniform2iv.
 func (ul UniformLocation2i) Uniform2iv(count int32, value *int32) {
 	gl.Uniform2iv(int32(ul), count, value)
 }
 
-//Uniform3iv is an alias for the Opengl function of the same name.
+//Uniform3iv is an alias to glUniform3iv.
 func (ul UniformLocation3i) Uniform3iv(count int32, value *int32) {
 	gl.Uniform3iv(int32(ul), count, value)
 }
 
-//Uniform4iv is an alias for the Opengl function of the same name.
+//Uniform4iv is an alias to glUniform4iv.
 func (ul UniformLocation4i) Uniform4iv(count int32, value *int32) {
 	gl.Uniform4iv(int32(ul), count, value)
 }
 
-//Uniform1uiv is an alias for the Opengl function of the same name.
+//Uniform1uiv is an alias to glUniform1uiv.
 func (ul UniformLocation1ui) Uniform1uiv(count int32, value *uint32) {
 	gl.Uniform1uiv(int32(ul), count, value)
 }
 
-//Uniform2uiv is an alias for the Opengl function of the same name.
+//Uniform2uiv is an alias to glUniform2uiv.
 func (ul UniformLocation2ui) Uniform2uiv(count int32, value *uint32) {
 	gl.Uniform2uiv(int32(ul), count, value)
 }
 
-//Uniform3uiv is an alias for the Opengl function of the same name.
+//Uniform3uiv is an alias to glUniform3uiv.
 func (ul UniformLocation3ui) Uniform3uiv(count int32, value *uint32) {
 	gl.Uniform3uiv(int32(ul), count, value)
 }
 
-//Uniform4uiv is an alias for the Opengl function of the same name.
+//Uniform4uiv is an alias to glUniform4uiv.
 func (ul UniformLocation4ui) Uniform4uiv(count int32, value *uint32) {
 	gl.Uniform4uiv(int32(ul), count, value)
 }
 
-//UniformMatrix2fv is an alias for the Opengl function of the same name.
+//UniformMatrix2fv is an alias to glUniformMatrix2fv.
 func (ul UniformLocationMatrix2) UniformMatrix2fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix2fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix3fv is an alias for the Opengl function of the same name.
+//UniformMatrix3fv is an alias to glUniformMatrix3fv.
 func (ul UniformLocationMatrix3) UniformMatrix3fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix3fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix4fv is an alias for the Opengl function of the same name.
+//UniformMatrix4fv is an alias to glUniformMatrix4fv.
 func (ul UniformLocationMatrix4) UniformMatrix4fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix4fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix2x3fv is an alias for the Opengl function of the same name.
+//UniformMatrix2x3fv is an alias to glUniformMatrix2x3fv.
 func (ul UniformLocationMatrix2x3) UniformMatrix2x3fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix2x3fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix3x2fv is an alias for the Opengl function of the same name.
+//UniformMatrix3x2fv is an alias to glUniformMatrix3x2fv.
 func (ul UniformLocationMatrix3x2) UniformMatrix3x2fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix3x2fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix2x4fv is an alias for the Opengl function of the same name.
+//UniformMatrix2x4fv is an alias to glUniformMatrix2x4fv.
 func (ul UniformLocationMatrix2x4) UniformMatrix2x4fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix2x4fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix4x2fv is an alias for the Opengl function of the same name.
+//UniformMatrix4x2fv is an alias to glUniformMatrix4x2fv.
 func (ul UniformLocationMatrix4x2) UniformMatrix4x2fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix4x2fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix3x4fv is an alias for the Opengl function of the same name.
+//UniformMatrix3x4fv is an alias to glUniformMatrix3x4fv.
 func (ul UniformLocationMatrix3x4) UniformMatrix3x4fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix3x4fv(int32(ul), count, transpose, value)
 }
 
-//UniformMatrix4x3fv is an alias for the Opengl function of the same name.
+//UniformMatrix4x3fv is an alias to glUniformMatrix4x3fv.
 func (ul UniformLocationMatrix4x3) UniformMatrix4x3fv(count int32, transpose bool, value *float32) {
 	gl.UniformMatrix4x3fv(int32(ul), count, transpose, value)
 }
